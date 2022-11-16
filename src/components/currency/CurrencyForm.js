90914e723fdd08e026e0e78414bc10bcc74d57f0
ArtemiyPercev <artemiypercev@gmail.com>
2022-11-17 01:37:53 +0200

    partly style of select was done

--- src/components/currency/CurrencyForm.js
+++ src/components/currency/CurrencyForm.js
@@ -2,15 +2,17 @@ import { HiOutlineArrowsRightLeft } from 'react-icons/hi2'
 import SelectCurrency from './SelectCurrency'
 import styles from './currencyForm.module.css'
 import Button from '../UI/Button'
+import { useState } from 'react'
 
 function CurrencyForm() {
+  const [selected, setSelected] = useState('EUR')
   return (
     <div className={styles.currenyForm}>
       <form>
         <div className={styles.currencyForm__select_currency}>
-          <SelectCurrency text="From" />
+          <SelectCurrency selected={selected} setSelected={setSelected} />
           <HiOutlineArrowsRightLeft />
-          <SelectCurrency text="To" />
+          <SelectCurrency selected={selected} setSelected={setSelected} />
         </div>
         <div>
           <input type="number" />
--- src/components/currency/SelectCurrency.js
+++ src/components/currency/SelectCurrency.js
@@ -1,20 +1,64 @@
 import Flag from 'react-world-flags'
-import Select from 'react-select'
 import styles from './SelectCurrency.module.css'
+import { useState } from 'react'
 
-function SelectCurrency({ children, text }) {
-  const options = [
-    { value: 'eur', label: 'EUR' },
-    { value: 'pln', label: 'PLN' },
-    { value: 'gbp', label: 'GBP' },
-  ]
+function SelectCurrency({ selected, setSelected }) {
+  const [isActive, setIsActive] = useState(false)
+
+  const options = ['EUR', 'PLN', 'GBP']
+
   return (
-    <div className={styles.select}>
-      <h4>{text}</h4>
-      <Select options={options} className={styles.select__currency} />
-      {/* <Flag code="pol" height="16" /> */}
+    <div className={styles.dropdown}>
+      <div
+        className={styles.dropdown_btn}
+        onClick={(e) => setIsActive(!isActive)}
+      >
+        {selected}
+      </div>
+      {isActive && (
+        <div className={styles.dropdown_content}>
+          {options.map((option) => (
+            <div
+              onClick={(e) => {
+                setSelected(option)
+                setIsActive(false)
+              }}
+              className={styles.dropdown_item}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
-
 export default SelectCurrency
+
+// return (
+//   <div>
+//     <div class={styles.select_container}>
+//       <div class={styles.option_container}></div>
+//       <div class={styles.select}>
+//         <input
+//           type="text"
+//           id="input"
+//           placeholder="select"
+//           onfocus="this.blur();"
+//         />
+//       </div>
+//       {options.map((option) => {
+//         return (
+//           <>
+//             <div class={styles.option}>
+//               <label>
+//                 <Flag code={option.value} height="16" />
+//                 <span>{option.label}</span>
+//               </label>
+//             </div>
+//           </>
+//         )
+//       })}
+//     </div>
+//   </div>
+// )
